bed1ae998652742eb2c52f7066929e3cf358b173
Rishop Babu <rishop.babu@optisolbusiness.com>
2023-08-05 09:18:07 -0600

    Initial Commit

---
+++ AuthenticationComponent/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AuthenticationComponent
+//
+//  Created by Mac-OBS-51 on 05/08/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
